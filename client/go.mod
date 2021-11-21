a3eb4b9aae67c99ad78d906c1f455355a3eb1477
teaglebuilt <dillan.teagle.va@gmail.com>
2021-11-21 09:17:49 -0600

    setup client

---
+++ client/go.mod
@@ -0,0 +1,12 @@
+module github.com/teaglebuilt/homelab
+
+go 1.17
+
+require (
+	github.com/fluxcd/helm-controller/api v0.13.0 // indirect
+	github.com/fluxcd/image-automation-controller/api v0.17.1 // indirect
+	github.com/fluxcd/image-reflector-controller/api v0.13.2 // indirect
+	github.com/fluxcd/kustomize-controller/api v0.18.0 // indirect
+	github.com/fluxcd/notification-controller/api v0.18.1 // indirect
+	github.com/fluxcd/source-controller/api v0.18.0 // indirect
+)
--- scripts/bootstrap_flux.sh
+++ scripts/bootstrap_flux.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
+REPO_OWNER=${REPO_OWNER:-teaglebuilt}
+REPO_NAME=${REPO_NAME:-homelab}
+REPO_BRANCH=${REPO_BRANCH:-master}
+REPO_PATH=${REPO_PATH:-kubernetes/clusters/dev}
+
+
 if [ -z "${GITHUB_TOKEN}" ]; then
     echo "set the GITHUB_TOKEN envvar to a Personal Access Token (PAT) of your account in order to bootstrap. This token will be used by flux to add a deploy key to the homeops repository"
     exit 1
 fi
 
-REPO_OWNER=${REPO_OWNER:-teaglebuilt}
-REPO_NAME=${REPO_NAME:-homelab}
-REPO_BRANCH=${REPO_BRANCH:-master}
-REPO_PATH=${REPO_PATH:-kubernetes/clusters/dev}
-NAMESPACE=${NAMESPACE:-homeops}
 
-
+echo "bootstap homeops in flux-system namespace"
 flux bootstrap github \
      --owner "${REPO_OWNER}" \
      --repository "${REPO_NAME}" \
      --branch "${REPO_BRANCH}" \
      --path "${REPO_PATH}" \
+     --network-policy=false \
+     --watch-all-namespaces=true \
+     --namespace=flux-system \
      --personal
